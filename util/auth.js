831fdeaf04803d6e0a180c636a6dd42e457b7e04
Ahmed Arif Sandhu <ahmedsandhu293@gmail.com>
2024-09-13 16:18:48 +0500

    refactor: added .env file

--- util/auth.js
+++ util/auth.js
@@ -1,22 +1,24 @@
 import axios from "axios";
+import { API_KEY } from "@env";
 
-const API_KEY = "AIzaSyA7CRT6jrFpwWYZwy2NTcViiX8dUJfSBtg";
-
-const authenticate = async(mode, email, password) => {
-  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+const authenticate = async (mode, email, password) => {
+  console.log(API_KEY);
+  const url =
+    `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=` + API_KEY;
+  console.log(url);
   const response = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true,
   });
-  const token = response.data.idToken
-  return token
+  const token = response.data.idToken;
+  return token;
 };
 
 export const createUser = (email, password) => {
- return authenticate('signUp', email, password)
+  return authenticate("signUp", email, password);
 };
 
-export const login = (email,password)=>{
-  return  authenticate('signInWithPassword', email, password)
-}
+export const login = (email, password) => {
+  return authenticate("signInWithPassword", email, password);
+};
